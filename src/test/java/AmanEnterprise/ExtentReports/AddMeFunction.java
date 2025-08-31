a923e7dfdfcdfa31bc112e0eaf8f5845be8a1ec4
Aman Sharma <aaaman.sh007@gmail.com>
2025-08-31 14:57:05 +0530

    main user -> added method to sum 2 numbers

--- src/test/java/AmanEnterprise/ExtentReports/AddMeFunction.java
+++ src/test/java/AmanEnterprise/ExtentReports/AddMeFunction.java
@@ -4,6 +4,10 @@ public class AddMeFunction {
     public static void main(String[] args) {
         int x = 10;
         int y = 20;
-        System.out.println(x+y);
+        System.out.println(addMethod(x,y));
+    }
+
+    public static Integer addMethod(int x, int y){
+        return (x+y);
     }
 }
